b0aeacf95cdf99c3cd4e650225d1ec786ec254d5
ZarulZakuanWSLHome <zarulzakuan@gmail.com>
2020-04-18 18:17:25 +0800

    init

--- js/getter.js
+++ js/getter.js
@@ -5,19 +5,23 @@ function getEntry() {
             var entryInfo = JSON.parse(xhr.responseText);
 
             console.log(entryInfo);
-            var modal = document.querySelector('.modal');  // assuming you have only 1
-            var html = document.querySelector('html');
-            modal.classList.add('is-active');
-            html.classList.add('is-clipped');
 
-            modal.querySelector('.modal-background').addEventListener('click', function (e) {
-                e.preventDefault();
-                modal.classList.remove('is-active');
-                html.classList.remove('is-clipped');
-            });
         }
     };
 
     xhr.open('GET', 'http://localhost:8000/entry/10000');
     xhr.send();
+}
+
+function toggleCameraModal() {
+    var modal = document.querySelector('#cameraModal');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+
+    modal.querySelector('.modal-background').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
 }
