de95dc590b6a8d555e8e4faa02a9a2a6cb1d7d0d
ZarulZakuanWSLHome <zarulzakuan@gmail.com>
2020-04-18 18:26:34 +0800

    init

---
+++ js/helper.js
@@ -0,0 +1,12 @@
+function toggleCameraModal() {
+    var modal = document.querySelector('#cameraModal');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+
+    modal.querySelector('.modal-background').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+}
