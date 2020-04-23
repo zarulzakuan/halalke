59ea2fdea2119f1adaa5ec469f882c5d25682005
Zarul Izuan Ahmad Zakuan <zarul-izuan.i.ahmad-zakuan@gsk.com>
2020-04-23 16:54:35 +0800

    update

--- js/auth.js
+++ js/auth.js
@@ -1,50 +1,52 @@
 window.addEventListener('load', function () {
-    var lock = new Auth0Lock('udGE155TdWI7SL0ZM2w316kMYRFHSpLM', 'isithalaldev.auth0.com');
 
     document.getElementById('btn-login').addEventListener('click', function () {
         lock.show();
     });
 
-    var Auth = (function () {
-
-        var wm = new WeakMap();
-        var privateStore = {};
-        var lock;
-
-        function Auth() {
-            this.lock = new Auth0Lock('udGE155TdWI7SL0ZM2w316kMYRFHSpLM', 'isithalaldev.auth0.com');
-            wm.set(privateStore, {
-                appName: "isithalaldev"
-            });
-        }
-
-        Auth.prototype.getProfile = function () {
-            return wm.get(privateStore).profile;
-        };
-
-        Auth.prototype.authn = function () {
-            // Listening for the authenticated event
-            this.lock.on("authenticated", function (authResult) {
-                // Use the token in authResult to getUserInfo() and save it if necessary
-                this.getUserInfo(authResult.accessToken, function (error, profile) {
-                    if (error) {
-                        // Handle error
-                        return;
-                    }
-
-                    //we recommend not storing Access Tokens unless absolutely necessary
-                    wm.set(privateStore, {
-                        accessToken: authResult.accessToken
-                    });
-
-                    wm.set(privateStore, {
-                        profile: profile
-                    });
-
-                });
-            });
-        };
-        return Auth;
-    }());
+    
 });
 
+var Auth = (function () {
+
+    var wm = new WeakMap();
+    var privateStore = {};
+    var lock;
+    
+    function Auth() {
+        this.lock = new Auth0Lock('udGE155TdWI7SL0ZM2w316kMYRFHSpLM', 'isithalaldev.auth0.com');
+        wm.set(privateStore, {
+            appName: "isithalaldev"
+        });
+    }
+
+    Auth.prototype.getProfile = function () {
+        return wm.get(privateStore).profile;
+    };
+
+    Auth.prototype.authn = function () {
+        // Listening for the authenticated event
+        this.lock.on("authenticated", function (authResult) {
+            console.log("authenticated");
+            console.log(authResult);
+            // Use the token in authResult to getUserInfo() and save it if necessary
+            this.getUserInfo(authResult.accessToken, function (error, profile) {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+
+                //we recommend not storing Access Tokens unless absolutely necessary
+                wm.set(privateStore, {
+                    accessToken: authResult.accessToken
+                });
+
+                wm.set(privateStore, {
+                    profile: profile
+                });
+
+            });
+        });
+    };
+    return Auth;
+}());
