2f07f805482e44a2ad61b051b955858f4bad05ec
Zarul Izuan Ahmad Zakuan <zarul-izuan.i.ahmad-zakuan@gsk.com>
2020-04-23 17:14:08 +0800

    added autho\0

--- js/auth.js
+++ js/auth.js
@@ -1,53 +1,25 @@
 window.addEventListener('load', function () {
     var lock = new Auth0Lock('udGE155TdWI7SL0ZM2w316kMYRFHSpLM', 'isithalaldev.auth0.com');
+    var accessToken = null;
+    var profile = null;
 
+    lock.on("authenticated", function (authResult) {
+        console.log("Authenticated!")
+        lock.getUserInfo(authResult.accessToken, function (error, profileResult) {
+            if (error) {
+                // Handle error
+                return;
+            }
+
+            accessToken = authResult.accessToken;
+            profile = profileResult;
+
+            // Update DOM
+        });
+    });
     document.getElementById('btn-login').addEventListener('click', function () {
         lock.show();
     });
 
-    
+
 });
-
-var Auth = (function () {
-
-    var wm = new WeakMap();
-    var privateStore = {};
-    var lock;
-    
-    function Auth() {
-        this.lock = new Auth0Lock('udGE155TdWI7SL0ZM2w316kMYRFHSpLM', 'isithalaldev.auth0.com');
-        wm.set(privateStore, {
-            appName: "isithalaldev"
-        });
-    }
-
-    Auth.prototype.getProfile = function () {
-        return wm.get(privateStore).profile;
-    };
-
-    Auth.prototype.authn = function () {
-        // Listening for the authenticated event
-        this.lock.on("authenticated", function (authResult) {
-            console.log("authenticated");
-            console.log(authResult);
-            // Use the token in authResult to getUserInfo() and save it if necessary
-            this.getUserInfo(authResult.accessToken, function (error, profile) {
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-
-                //we recommend not storing Access Tokens unless absolutely necessary
-                wm.set(privateStore, {
-                    accessToken: authResult.accessToken
-                });
-
-                wm.set(privateStore, {
-                    profile: profile
-                });
-
-            });
-        });
-    };
-    return Auth;
-}());
